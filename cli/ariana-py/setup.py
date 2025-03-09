8ff14f190177de12c4e17b14da5002633f526e6c
Anicet Nougaret <an.nougaret@gmail.com>
2025-03-08 16:22:28 -0800

    new ariana-npm & pip

--- cli/ariana-py/setup.py
+++ cli/ariana-py/setup.py
@@ -4,8 +4,8 @@ import platform
 
 setup(
     name='ariana',
-    version='0.2.1',
-    description='Ariana CLI - A tool for code instrumentalization and execution with observability',
+    version='0.2.3',
+    description='Debug your JS/TS/Python code in development way faster than with a traditional debugger',
     packages=['ariana'],
     package_data={
         'ariana': ['bin/ariana-linux-x64', 'bin/ariana-macos-x64', 'bin/ariana-macos-arm64', 'bin/ariana-windows-x64.exe'],
